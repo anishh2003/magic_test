ba7052a1d113838dba34ac8fb4f5f0006474e1a4
Anish <anishh2003@gmail.com>
2025-08-15 11:41:01 +0100

    bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:magic_test/feature/workoutList/bloc/workout_list_bloc.dart';
+import 'package:magic_test/router/router.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  runApp(
+    BlocProvider(
+      create: (_) => WorkoutListBloc()..add(LoadWorkouts()),
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Workout Tracker',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      routerConfig: appRouter,
     );
   }
 }
